a2259d2acaabfd3d8754916b663c03a98f47a1e4
Ricardo042004 <silvalimaricardo020@gmail.com>
2025-04-28 15:02:51 +0000

    23

--- lista2.py
+++ lista2.py
@@ -246,6 +246,49 @@ def q18():
 #Peixe          230cal Sorvete diet     110cal Suco de laranja   70cal
 #Frango         250cal Mousse diet      170cal Suco de melão     100cal
 #Carne          350cal Mousse chocolate 200cal Refrigerante diet 65cal
+def q23():
+    prato = int(input('''
+        Pratos:
+    1 - Vegetariano
+    2 - Peixe
+    3 - Frango
+    4 - Carne
+    Digite a opção desejada:
+    '''))
+
+    bebida = int(input('''
+    Bebidas:
+    1 - Chá
+    2 - Suco de laranja
+    3 - Suco de melão
+    4 - Refrigerante diet
+    Digite a opção desejada:
+    '''))
+    
+    sobremesa = int(input( '''
+    Sobresemas: 
+    1 - Abacaxi
+    2 - Sorvete diet
+    3 - Mousse diet
+    4 - Mousse chocolate
+    Digite a opção desejada:
+    '''))
+
+    cal = 0
+    cal += 180 if prato == 1 else 0
+    cal += 230 if prato == 2 else 0
+    cal += 250 if prato == 3 else 0
+    cal += 350 if prato == 4 else 0    
+    cal += 20 if bebida == 1 else 0
+    cal += 70 if bebida == 2 else 0
+    cal += 100 if bebida == 3 else 0
+    cal += 65 if bebida == 4 else 0    
+    cal += 75 if sobremesa == 1 else 0
+    cal += 110 if sobremesa == 2 else 0
+    cal += 170 if sobremesa == 3 else 0
+    cal += 200 if sobremesa == 4 else 0
+
+    print(f'Total de calorias: {cal}')
 
 #24. A polícia rodoviária resolveu fazer cumprir a lei e vistoriar veículos para
 #cobrar dos motoristas o DUT. Sabendo-se que o mês em que o emplacamento do
